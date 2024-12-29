1c592a68a0630a8fff91938365ad005d5486b1b7
Dennis <dennisapazalopez@gmail.com>
2024-12-28 19:25:41 -0600

    klajfl

--- practica1.py
+++ practica1.py
@@ -1,2 +1,5 @@
-print ("Hola mundo y gordito")
-print ("hola a todos")
+print ("programa para imprimir la suma de dos numeros")
+numero =int(input("ingreeser el primer numero: "))
+numero_2 = int(input("ingresar el segundo numero: "))
+resultado = numero + numero_2
+print ("El resultado es:", resultado)
