eb16d73a1db8d03cace0052e85343f3108887e4e
Dennis <dennisapazalopez@gmail.com>
2024-12-30 14:54:16 -0600

    primera prueba

---
+++ practica2.py
@@ -0,0 +1,7 @@
+print ("Este es un programa para convertir de segundos a minutos y horas")
+segundos = float(input("ingrese los segundos que desea convertir"))
+minutos = segundos/60
+horas = minutos / 60
+print ("los segundos son: ", segundos)
+print ("en minutos son: ", minutos)
+print ("En horas son: ", horas)
