80ab728668f89da63b9beb3196aeeb26010b6507
Dennis <dennisapazalopez@gmail.com>
2024-12-30 15:13:26 -0600

    copia 1 de practica 3

---
+++ practica3.py
@@ -0,0 +1,15 @@
+print ("Leer un numero y averiguar su cuadrado, cubo, raiz cuadrada, raiz cubica ")
+numero = int(input("ingrese un numero:"))
+cuadrado = numero**2
+cubo = numero**3
+raiz_cuadrada= (numero)**1/2
+raiz_cubica = (numero)**1/3
+numero =str(numero)
+cuadrado = str(cuadrado)
+cubo =str (cubo)
+raiz_cuadrada= str(raiz_cuadrada)
+raiz_cubica = str(raiz_cubica)
+print ("El cuadrado de "+numero+ " es : "+cuadrado )
+print ("El cubo de" + numero + " es: "+ cubo)
+print ("La raiz cuadrada de " + numero + " es: " + raiz_cuadrada)
+print ("La raiz cubica de " + numero + " es: " + raiz_cubica)
