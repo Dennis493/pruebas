7b0295f0bc29ec0078f3959f62cb930ae8d7a559
Dennis <dennisapazalopez@gmail.com>
2024-12-30 19:38:25 -0600

    respaldo

---
+++ practica4.py
@@ -0,0 +1,9 @@
+print ("hacer leer dos numeros y del mayor retarle el menor")
+numero1= int(input("introduce un numero: "))
+numero2 = int(input("introduce otro numero: "))
+if numero1 > numero2 :
+    resultado = numero1 - numero2
+    print ("El resultado es:", resultado)
+else :
+    resultado2 = numero2 - numero1
+    print ("El resultado es: ", resultado2)
